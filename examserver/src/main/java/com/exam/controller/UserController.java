4b11cec7f254e52855f8ad2b6f62908e1a7b89ad
Santosh Kumar <your_email@example.com>
2022-10-21 14:43:58 +0530

    added jwt spring Security

--- examserver/src/main/java/com/exam/controller/UserController.java
+++ examserver/src/main/java/com/exam/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.exam.service.UserService;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin("*")
 public class UserController {
 	@Autowired
 	private UserService userService;
@@ -26,6 +28,8 @@ public class UserController {
 	//creating user 
 	@PostMapping("/")
 	public User createUser(@RequestBody User user) {
+		
+		user.setProfile("default.png");
 		
 		Set<UserRole> roles=new HashSet<>();
 		
